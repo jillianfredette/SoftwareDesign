e98c1f62bf6c3decdf6e47d5c05f3b87c7daa2c0
Miguel Rodriguez <miguelarodriguez7594@gmail.com>
2019-09-23 00:42:23 -0500

    Updated the ProjectTree to be able to handle directories with multiple files and folders

--- Code_Editor/src/ProjectTree.java
+++ Code_Editor/src/ProjectTree.java
@@ -1,31 +1,20 @@
-
 //https://docs.oracle.com/javase/tutorial/uiswing/examples/components/TreeDemoProject/src/components/TreeDemo.java
 
 import javax.swing.*;
-import javax.swing.plaf.TabbedPaneUI;
-import javax.swing.plaf.basic.BasicTabbedPaneUI;
 import javax.swing.tree.*;
-import javax.swing.plaf.metal.*;
 import java.awt.*;
 import java.io.*;
 import java.awt.event.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.nio.file.Path;
-import java.util.concurrent.TimeUnit;
-import java.net.URL;
 import javax.swing.event.*;
-import javax.swing.plaf.basic.*;
+
 
 public class ProjectTree extends JPanel implements TreeSelectionListener {
-    private JEditorPane htmlPane;
     private JTree tree;
-    private URL helpURL;
-    private static boolean DEBUG = false;
     DefaultMutableTreeNode top;
-
+    private DefaultTreeModel treeModel;
     //Optionally set the look and feel.
     private static boolean useSystemLookAndFeel = false;
 
@@ -41,13 +30,19 @@ public class ProjectTree extends JPanel implements TreeSelectionListener {
         ProjectName = theProjectName;
         ProjectPath = theProjectPath;
         theCodeEditorFrame = theMainFrame;
+        File fileRoot = new File(ProjectPath);
 
-        //Create the nodes.
-        top = new DefaultMutableTreeNode(ProjectName);
-        createNodes(top);
+        // Creates the nodes from the directory's path
+        top = new DefaultMutableTreeNode(new FileNode(fileRoot));
+        treeModel = new DefaultTreeModel(top);
 
-        //Create a tree that allows one selection at a time.
-        tree = new JTree(top);
+        // Creates a tree that allows one selection at a time.
+        tree = new JTree(treeModel);
+
+        // Creates the children nodes, doesn't matter if its a file or a folder
+        ChildNode ccn = new ChildNode(fileRoot, top);
+        new Thread(ccn).start();
+
         tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
 
         //Listen for when the selection changes.
@@ -87,22 +82,6 @@ public class ProjectTree extends JPanel implements TreeSelectionListener {
 
         }
     }
-
-    // Creates Nodes for Tree
-    private void createNodes(DefaultMutableTreeNode top) {
-        DefaultMutableTreeNode folder = null;
-        DefaultMutableTreeNode file = null;
-
-        folder = new DefaultMutableTreeNode("src");
-        top.add(folder);
-
-        //original Tutorial
-        file = new DefaultMutableTreeNode("Main.java");
-        folder.add(file);
-
-    }
-
-
 
     // Popup Specifics
     class TreePopup extends JPopupMenu {
@@ -348,154 +327,3 @@ public class ProjectTree extends JPanel implements TreeSelectionListener {
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-/*  BACKUP
-
-//https://docs.oracle.com/javase/tutorial/uiswing/examples/components/TreeDemoProject/src/components/TreeDemo.java
-
-import javax.swing.*;
-import javax.swing.tree.*;
-import javax.swing.plaf.metal.*;
-import java.awt.*;
-import java.io.*;
-import java.awt.event.*;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.nio.file.Path;
-import java.util.concurrent.TimeUnit;
-import java.net.URL;
-import javax.swing.event.*;
-
-public class ProjectTree extends JPanel implements TreeSelectionListener {
-    private JEditorPane htmlPane;
-    private JTree tree;
-    private URL helpURL;
-    private static boolean DEBUG = false;
-    DefaultMutableTreeNode top;
-
-    //Optionally set the look and feel.
-    private static boolean useSystemLookAndFeel = false;
-
-    public ProjectTree() {
-        super(new GridLayout(1, 0));
-
-        //Create the nodes.
-        top = new DefaultMutableTreeNode("Project name");
-        createNodes(top);
-
-        //Create a tree that allows one selection at a time.
-        tree = new JTree(top);
-        tree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
-
-        //Listen for when the selection changes.
-        tree.addTreeSelectionListener(this);
-
-        //Create the scroll pane and add the tree to it.
-        JScrollPane treeView = new JScrollPane(tree);
-
-        //Add the tree view to this panel.
-        add(treeView);
-
-
-        // Show Popup on Tree
-        final TreePopup treePopup = new TreePopup(tree);
-        tree.addMouseListener(new MouseAdapter() {
-            public void mouseReleased(MouseEvent e) {
-                if(e.getButton() == MouseEvent.BUTTON3) {
-                    treePopup.show(e.getComponent(), e.getX(), e.getY());
-                    System.out.println(e.getComponent());
-                }
-            }
-        });
-    }
-
-    // Required by TreeSelectionListener interface.
-
-    public void valueChanged(TreeSelectionEvent e) {
-        DefaultMutableTreeNode node = (DefaultMutableTreeNode)
-                tree.getLastSelectedPathComponent();
-
-        if (node == null) return;
-
-        Object nodeInfo = node.getUserObject();
-        if (node.isLeaf()) {
-
-        } else {
-
-        }
-    }
-
-    // Creates Nodes for Tree
-    private void createNodes(DefaultMutableTreeNode top) {
-        DefaultMutableTreeNode folder = null;
-        DefaultMutableTreeNode file = null;
-
-        folder = new DefaultMutableTreeNode("Project Folder");
-        top.add(folder);
-
-        //original Tutorial
-        file = new DefaultMutableTreeNode("File");
-        folder.add(file);
-
-    }
-
-    // Popup Specifics
-    class TreePopup extends JPopupMenu {
-        public TreePopup(JTree tree) {
-            JMenuItem delete = new JMenuItem("Delete");
-            JMenuItem add = new JMenuItem("Add");
-            JTextField field1 = new JTextField("File Name");
-            delete.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent ae) {
-                    System.out.println("Delete child");
-                    DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) tree.getSelectionPath().getLastPathComponent();
-                    DefaultTreeModel model = (DefaultTreeModel) tree.getModel();
-                    model.removeNodeFromParent(selectedNode);
-                }
-            });
-            add.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent ae) {
-//                    System.out.println("Add child");
-//                    DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) tree.getSelectionPath().getLastPathComponent();
-//                    JFrame addNodeFrame;
-//                    addNodeFrame = new JFrame("Add File");
-//                    addNodeFrame.add(field1);
-//                    DefaultMutableTreeNode newNode = new DefaultMutableTreeNode(field1.getText());
-//                    selectedNode.add(newNode);
-
-                }
-            });
-            add(add);
-            add(new JSeparator());
-            add(delete);
-        }
-    }
-
-
-    public void deleteDirectory(Path path) throws IOException {
-
-    }
-
-    public void setProjectName(String name) {
-        top.setUserObject(name);
-        ((DefaultTreeModel) tree.getModel()).nodeChanged(top);
-
-    }
-
-
-}
-
-
- */
