2533d17bcd87aa8777649dde1641eade654d007d
kevin huynh <kevinhuynh269@yahoo.com>
2019-10-27 20:26:11 -0500

    added some color to the text

--- Code_Editor/src/CodeField.java
+++ Code_Editor/src/CodeField.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.plaf.metal.*;
+import javax.swing.text.*;
 import java.awt.*;
 import java.io.*;
 import java.awt.event.*;
@@ -11,12 +12,16 @@ import java.awt.event.KeyListener;
 import java.util.concurrent.TimeUnit;
 
 
-public class CodeField extends JEditorPane implements ActionListener, KeyListener {
+public class CodeField extends JTextPane implements ActionListener, KeyListener {
 
     private String ProjectName;
     private String ProjectPath;
     private String ProjectSDKPath;
     private String FileName;
+
+    public StyleContext context = new StyleContext();
+   public  Style style = context.addStyle("test", null);
+
 
     public CodeField(String theProjectName, String theProjectPath, String theFileName) {
         super();
@@ -28,8 +33,6 @@ public class CodeField extends JEditorPane implements ActionListener, KeyListene
         this.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.lightGray));
         this.setPreferredSize(new Dimension(700, 1000));
         this.addKeyListener(this);
-
-
 
 
 //        JScrollPane scroll = new JScrollPane(this);
@@ -49,7 +52,7 @@ public class CodeField extends JEditorPane implements ActionListener, KeyListene
     }
 
     // Save Code by pressing Ctrl + S
-    public void keyPressed(KeyEvent e){
+    public void keyPressed(KeyEvent e) {
         if ((e.getKeyCode() == KeyEvent.VK_S) && ((e.getModifiers() & KeyEvent.CTRL_MASK) != 0)) {
 //        if ((e.getKeyCode() == KeyEvent.VK_F4)) {
             try {
@@ -69,6 +72,44 @@ public class CodeField extends JEditorPane implements ActionListener, KeyListene
     }
 
     public void keyTyped(KeyEvent e) {
+        String[] kevin = new String[10];
+        kevin[0] = "+";
+        kevin[1] = "-";
+        kevin[2] = "/";
+        kevin[3] = "||";
+        String length = this.getText();
+        int stringlength = length.length() - 1;
+
+        if (this.getText().charAt(stringlength) == '+' || this.getText().charAt(stringlength) == '-' || this.getText().charAt(stringlength) == '*' || this.getText().charAt(stringlength) == '/' || (this.getText().charAt(stringlength) == '|' && this.getText().charAt(stringlength - 1) == '|') || (this.getText().charAt(stringlength) == '&' && this.getText().charAt(stringlength - 1) == '&')) {
+            if ((this.getText().charAt(stringlength) == '|' && this.getText().charAt(stringlength - 1) == '|')) {
+                StyleConstants.setForeground(style, Color.red);
+                //CodeField.setForeground(Color.red);
+                this.getStyledDocument().setCharacterAttributes(stringlength - 1, stringlength, style, false);
+                //CodeField.setForeground(Color.black);
+                StyleConstants.setForeground(style, Color.red); // changes it to red
+                this.setForeground(Color.black);
+            }
+            if ((this.getText().charAt(stringlength) == '&' && this.getText().charAt(stringlength - 1) == '&')) {
+                StyleConstants.setForeground(style, Color.red);
+                //CodeField.setForeground(Color.red);
+
+                this.getStyledDocument().setCharacterAttributes(stringlength - 1, stringlength, style, false);
+                //CodeField.setForeground(Color.black);
+                StyleConstants.setForeground(style, Color.red); // changes it to red
+                this.setForeground(Color.black);
+            }
+            StyleConstants.setForeground(style, Color.red);
+            //CodeField.setForeground(Color.red);
+
+            this.getStyledDocument().setCharacterAttributes(stringlength, stringlength, style, false);
+            //CodeField.setForeground(Color.black);
+            StyleConstants.setForeground(style, Color.red); // changes it to red
+            this.setForeground(Color.black);
+            StyleConstants.setForeground(style, Color.black);
+
+
+        }
+
 
     }
 }
